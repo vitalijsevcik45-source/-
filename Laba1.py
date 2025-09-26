4d645b1c138a45f6e8ab2739f2a21944e040450d
vitalijsevcik45-source <vitalijsevcik45@gmail.com>
2025-09-26 13:15:19 +0300

    Create Laba1.py

---
+++ Laba1.py
@@ -0,0 +1,34 @@
+count = 23
+distance = 23.2
+city = 'Arizona'
+is_cool = True
+titles = ["Silver", "Golden"]
+coords = (4321, 42, -4568)
+members = {"Vitalik": "Vlad"}
+nothing = None
+unique_nums = {1, 2, 3, 4}
+binary_data = b"Hi"
+
+
+print(binary_data)
+print(unique_nums)
+print(nothing)
+print(members)
+print(coords)
+print(titles)
+print(is_cool)
+print(city)
+print(distance)
+print(count)
+
+
+print(type(binary_data))
+print(type(unique_nums))
+print(type(nothing))
+print(type(members))
+print(type(coords))
+print(type(titles))
+print(type(is_cool))
+print(type(city))
+print(type(distance))
+print(type(count))
